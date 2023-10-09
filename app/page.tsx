93fae24692b0fb8187c64b4d669c2375a94783ba
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-09 00:54:08 -0300

    feat: Add home page and stylization

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,117 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import useSendEmail from '@/hooks/useSendEmail';
+import Image from 'next/image';
+import { SetStateAction, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import styles from './page.module.css';
+import RightArrowIcon from '@/components/RightArrowIcon';
+
 
 export default function Home() {
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const { mutate, isError, isSuccess } = useSendEmail();
+
+  useEffect(() => {
+    if (!isSuccess) return;
+
+    toast.success("Assinatura confirmada!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      progress: undefined,
+      theme: "dark",
+    })
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (!isError) return;
+
+    toastErro("Erro ao confirmar! Verifique seu email e tente novamente.");
+
+  }, [isError])
+
+  const toastErro = (title: string) => {
+    toast.error(title, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      progress: undefined,
+      theme: "dark",
+    })
+  }
+
+  const limparCampos = () => {
+    const allInput = document.querySelectorAll('input');
+    allInput.forEach((input) => input.value = '')
+  }
+
+  const validarInfo = () => {
+
+    let re = /^(\w+[\.-]?\w+)@(\w+[\.-]?\w+)(\.\w{2,3})$/;
+
+    if (name.length == 0) {
+      toastErro("Nome vazio!");
+      return false;
+    }
+
+    if (!re.test(email) || email.length == 0) {
+      toastErro("Email inválido!");
+      return false;
+    }
+
+    limparCampos();
+
+    return true;
+  }
+
+  const submit = () => {
+
+    if (validarInfo()) {
+
+      mutate({
+        email: email,
+        name: name
+      })
+    }
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+      <Image className={styles.profilePic} width={150} height={150} src="https://avatars.githubusercontent.com/u/82993547?v=4" alt="Foto de perfil de Jeziel Almeida" />
+
+      <h1 className={styles.heading}>TechNow</h1>
+
+      <p className={styles.paragraph}>Fique por dentro de tudo que acontece no mundo da tecnologia!</p>
+
+      <div className={styles.inputWrapper}>
+        <input
+          className={styles.input}
+          type="text"
+          onChange={e => setName(e.target.value)}
+          placeholder='Digite seu nome'
+        />
+        <div className={styles.inputSubmit}>
+          <input
+            className={styles.input}
+            type="email"
+            onChange={e => setEmail(e.target.value)}
+            placeholder='Digite seu email'
+          />
+          <button onClick={submit} className={styles.btn}>
+            <RightArrowIcon />
+          </button>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
