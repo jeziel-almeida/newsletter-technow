6f74629ca3361f42ca691898dfe521c373646866
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-09 00:51:50 -0300

    feat: Add RightArrowIcon svg component

---
+++ components/RightArrowIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const RightArrowIcon = () => {
+    return (
+        <svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.1538 1L31 15.5L17.1538 30M29.0769 15.5L0.999999 15.5" stroke="url(#paint0_linear_1_13)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            <defs>
+                <linearGradient id="paint0_linear_1_13" x1="31" y1="15.5" x2="1" y2="15.5" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#52d6fe" />
+                    <stop offset="1" stopColor="#2fd7f5" />
+                </linearGradient>
+            </defs>
+        </svg>
+    )
+}
+
+export default RightArrowIcon
